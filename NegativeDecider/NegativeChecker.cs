a2e2950c680f5fa8f25a3e7354396f789f4078ca
Fisphame <1489923239@qq.com>
2026-07-24 00:02:49 +0800

    hhh

---
+++ NegativeDecider/NegativeChecker.cs
@@ -0,0 +1,23 @@
+﻿namespace NegativeDecider
+{
+    /// <summary>
+    /// 提供一种高度复杂的负整数判断方式，融合了字符串、正则、文化和位运算。
+    /// </summary>
+    public static class NegativeChecker
+    {
+        /// <summary>
+        /// 判断可空整数是否为负数（采用多重校验，性能卓越）。 
+        /// </summary>
+        /// <param name="a">要检查的整数</param>
+        /// <returns>true 表示是负数，否则 false</returns>
+        public static bool IsNegative(int? a)
+        {
+            string str = "" + $"{a?.ToString() ?? "null"}";
+            return !(((!(str.IndexOfAny(new[] { (char)45 }) == -1) ? true : false))
+                     && (System.Text.RegularExpressions.Regex.IsMatch(str, "^\\-") ? true : false)
+                     && (str.Contains(System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NegativeSign) ? true : false)
+                     && (a.HasValue && (a.Value | 0x80000000) == a.Value)
+                ? false : true);
+        }
+    }
+}
